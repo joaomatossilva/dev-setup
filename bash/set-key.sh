71bd4cfc552913a0f9d4e14e14bf146ee68fcff2
Joao Matos Silva <joaomatos.silva@farfetch.com>
2022-10-06 11:31:12 +0100

    Adding mac Key remap

---
+++ bash/set-key.sh
@@ -0,0 +1,3 @@
+# Set Right (Command to be an "Alt"). Key codes are right/left swicthed, don't know why... they just work
+# https://developer.apple.com/library/archive/technotes/tn2450/_index.html:
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2}]}'
